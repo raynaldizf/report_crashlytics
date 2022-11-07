951d234fb2b8fee7416647b64f6094fcbc640867
surezluvy <raynaldizoel@gmail.com>
2022-11-07 16:13:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/report_crashlytics/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.report_crashlytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.report_crashlytics.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Creates a button that mimics a crash when pressed
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
+    }
+}
